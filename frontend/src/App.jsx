ba46b121d9c4ef18b68be22cb731fefc6994876e
jayesh-borase <borsejayesh069@gmail.com>
2025-06-26 11:43:25 +0530

    AddEmployee page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import Homepage from "./Pages/Homepage"
-import {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
-function App() {
+import Homepage from "./Pages/Homepage";
+import AddEmp from "./Pages/AddEmp";
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
   return (
-   <Router>
-    <Routes>
-      <Route path="/" element = {<Homepage/>} />
-    </Routes>
-   </Router>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Home" element={<Homepage />} />
+        <Route path="/Add" element={<AddEmp />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
